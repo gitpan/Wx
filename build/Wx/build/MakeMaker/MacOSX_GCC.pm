cfa4d1f1ca5830337aa7ab9edd8209f3630d03c8
Mattia Barbon <mbarbon@users.sourceforge.net>
2006-06-18 16:19:57 +0000

    Import of MBARBON/Wx-0.49_07 from CPAN.
    gitpan-cpan-distribution: Wx
    gitpan-cpan-version:      0.49_07
    gitpan-cpan-path:         MBARBON/Wx-0.49_07.tar.gz
    gitpan-cpan-author:       MBARBON
    gitpan-cpan-maturity:     developer

--- build/Wx/build/MakeMaker/MacOSX_GCC.pm
+++ build/Wx/build/MakeMaker/MacOSX_GCC.pm
@@ -10,7 +10,7 @@ sub configure_core {
 
   $config{depend}{'$(INST_STATIC)'} .= ' wxPerl';
   $config{depend}{'$(INST_DYNAMIC)'} .= ' wxPerl';
-  $config{clean}{FILES} .= " wxPerl cpp/wxPerl.osx/build cpp/wxPerl.osx/wxPerl.c";
+  $config{clean}{FILES} .= " wxPerl cpp/wxPerl.osx/build cpp/wxPerl.osx/wxPerl.c cpp/wxPerl.osx/wxPerl.r";
 
   return %config;
 }
