4925d14efd72eabff321e77bd0953bd84a279968
Mattia Barbon <mbarbon@users.sourceforge.net>
2010-03-30 21:00:20 +0000

    Import of MBARBON/Wx-0.9701_01 from CPAN.
    gitpan-cpan-distribution: Wx
    gitpan-cpan-version:      0.9701_01
    gitpan-cpan-path:         MBARBON/Wx-0.9701_01.tar.gz
    gitpan-cpan-author:       MBARBON
    gitpan-cpan-maturity:     developer

--- Wx.pm
+++ Wx.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     01/10/2000
-## RCS-ID:      $Id: Wx.pm 2779 2010-02-06 08:49:13Z mbarbon $
+## RCS-ID:      $Id: Wx.pm 2867 2010-03-30 20:48:54Z mbarbon $
 ## Copyright:   (c) 2000-2010 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -21,7 +21,7 @@ use vars qw(@ISA $VERSION $XS_VERSION $AUTOLOAD @EXPORT_OK %EXPORT_TAGS
 $_msw = 1; $_gtk = 2; $_motif = 3; $_mac = 4; $_x11 = 5;
 
 @ISA = qw(Exporter);
-$VERSION = '0.9701';
+$VERSION = '0.9701_01';
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
