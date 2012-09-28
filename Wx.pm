a865f792774c68cf5c746c95e2912ed42507921b
Mark Dootson <mdootson@cpan.org>
2012-09-28 14:13:41 +0000

    Import of MDOOTSON/Wx-0.9913 from CPAN.
    gitpan-cpan-distribution: Wx
    gitpan-cpan-version:      0.9913
    gitpan-cpan-path:         MDOOTSON/Wx-0.9913.tar.gz
    gitpan-cpan-author:       MDOOTSON
    gitpan-cpan-maturity:     released

--- Wx.pm
+++ Wx.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     01/10/2000
-## RCS-ID:      $Id: Wx.pm 3358 2012-09-21 00:56:09Z mdootson $
+## RCS-ID:      $Id: Wx.pm 3373 2012-09-26 11:33:52Z mdootson $
 ## Copyright:   (c) 2000-2011 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -21,7 +21,7 @@ use vars qw(@ISA $VERSION $XS_VERSION $AUTOLOAD @EXPORT_OK %EXPORT_TAGS
 $_msw = 1; $_gtk = 2; $_motif = 3; $_mac = 4; $_x11 = 5;
 
 @ISA = qw(Exporter);
-$VERSION = '0.9912';
+$VERSION = '0.9913';
 $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;
 
